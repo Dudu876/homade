636961b87dd4260257b284f61ba25dde22dc86d1
Michael <michael.reizelman14@gmail.com>
2016-03-22 01:51:30 +0200

    bereshit

---
+++ Client/controllers/addMeal.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Dudu on 19/03/2016.
+ */
+
+homadeApp.controller('addMealCtrl', ['$scope', function ($scope) {
+
+    $scope.meal = {};
+
+    $scope.init = function (){
+
+    }
+
+}]);
