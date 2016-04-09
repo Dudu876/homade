acda4a511c70793c25cc0482023f53f966e15537
Dudu876 <dudu876@gmail.com>
2016-04-09 21:32:57 +0300

    trying to upload a file

--- Client/app.js
+++ Client/app.js
@@ -1,4 +1,4 @@
 /**
  * Created by Dudu on 08/03/2016.
  */
-var homadeApp = angular.module('homadeApp', ['ngRoute', 'appRoutes', 'ezfb', 'ngTagsInput', 'uiGmapgoogle-maps']);
+var homadeApp = angular.module('homadeApp', ['ngRoute', 'appRoutes', 'ezfb', 'ngTagsInput', 'uiGmapgoogle-maps', 'flow']);
