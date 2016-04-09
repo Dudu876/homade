0c07b5326d38c667fda343e0c69d7da537d6d410
Michael <michael.reizelman14@gmail.com>
2016-04-10 00:12:33 +0300

    add libraries

--- Server/models/order.js
+++ Server/models/order.js
@@ -1,18 +1,18 @@
-/**
- * Created by Dudu on 25/03/2016.
- */
-
 // grab the mongoose module
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var orderSchema = new Schema({
-
-    
-
+    clientFBId: Number,
+    meal: { type: Schema.Types.ObjectId, ref: 'Meal' },
+    chef: { type: Schema.Types.ObjectId, ref: 'Chef' },
+    rating: { service: Number, taste: Number, valueForPrice: Number },
+    quantity: Number,
+    totalPrice: Number,
+    date: Date
 });
 
-// define our meal model
+// define our order model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Order', orderSchema );
 
