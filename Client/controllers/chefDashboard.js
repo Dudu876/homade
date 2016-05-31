526bfa6842911f5b95c0c3f5314735e3b2db292e
Michael <michael.reizelman14@gmail.com>
2016-05-31 22:54:19 +0300

    Autocomplete

--- Client/controllers/chefDashboard.js
+++ Client/controllers/chefDashboard.js
@@ -12,7 +12,28 @@ homadeApp.controller('chefDashboardCtrl', ['$scope', 'ordersFactory', 'userFacto
 
         ordersFactory.getCompleteOrdersByChef(userFactory.fbId).success(function(data)
         {
+            $scope.completeOrders = data;
+            $scope.completeOrders.forEach(function(element, index, array) {
+                ezfb.api(element.clientFBId + '?fields=name', function (res) {
+                    if (!res.error)
+                    {
+                        element.clientName = res.name;
+                    }
+                });
+            });
+        });
 
+        ordersFactory.getActiveOrdersByChef(userFactory.fbId).success(function(data)
+        {
+            $scope.activeOrders = data;
+            $scope.activeOrders.forEach(function(element, index, array) {
+                ezfb.api(element.clientFBId + '?fields=name', function (res) {
+                    if (!res.error)
+                    {
+                        element.clientName = res.name;
+                    }
+                });
+            });
         });
     };
 
@@ -20,6 +41,29 @@ homadeApp.controller('chefDashboardCtrl', ['$scope', 'ordersFactory', 'userFacto
         getOrdersCount();
     };
 
+    $scope.formatDate = function(date){
+        var dateOut = new Date(date);
+        return dateOut;
+    };
+
+    $scope.fullStars = function(order) {
+        if (order.rating != null)
+        {
+            return new Array(order.rating);
+        }
+        else {
+            return new Array(0);
+        }
+    };
+
+    $scope.emptyStars = function(order) {
+        if (order.rating != null) {
+            return new Array(5 - order.rating);
+        }
+        else {
+            return new Array(5);
+        }
+    };
 
     if (userFactory.fbId != "") {
         init();
