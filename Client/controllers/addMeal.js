643ea40bedc9b939895e23225c6f41d44c2a1550
Dudu876 <dudu876@gmail.com>
2016-06-12 23:38:37 +0300

    add meal fix

--- Client/controllers/addMeal.js
+++ Client/controllers/addMeal.js
@@ -78,33 +78,37 @@ homadeApp.controller('addMealCtrl', ['$scope', '$uibModalInstance', 'mealFactory
         meal.chefFBId = userFactory.fbId;
 
         mealFactory.create(meal).success(function(data) {
-            alert ("meal saved!" + "  " + data);
+            //alert ("meal saved!" + "  " + data);
+            console.log("meal saved!" + "  " + data);
             meal._id = data;
-            $uibModalInstance.close({ 'meal': meal, 'isUpdate': isUpdate});
+            //$uibModalInstance.close({ 'meal': meal, 'isUpdate': isUpdate});
+            upload($scope.meal.file, meal); //call upload function
         }).error(function(data) {
             alert(data);
         });
-
-        upload($scope.meal.file); //call upload function
+        
     };
 
-    function upload(file) {
+    function upload(file, meal) {
         Upload.upload({
             url: '/upload', //webAPI exposed to upload the file
             data:{file:file} //pass file as data, should be user ng-model
         }).then(function (resp) { //upload function returns a promise
             if(resp.data.error_code === 0){ //validate success
-                alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
+                //alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
+                console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
             } else {
-                alert('an error occured');
+                //alert('an error occured');
+                console.log('an error occured');
             }
+            $uibModalInstance.close({ 'meal': meal, 'isUpdate': isUpdate});
         }, function (resp) { //catch error
             console.log('Error status: ' + resp.status);
-            alert('Error status: ' + resp.status);
+            //alert('Error status: ' + resp.status);
         }, function (evt) {
-            console.log(evt);
+            //console.log(evt);
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+            //console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
             $scope.meal.file.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
         });
     }
